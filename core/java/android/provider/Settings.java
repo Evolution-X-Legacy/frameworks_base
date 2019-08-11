18451f6934aa5549a010636e9ef079ad1f5aa1d5
Stallix <thestallix@gmail.com>
2019-08-11 19:49:39 +0530

    Revert "base: Introduce FontService"
    This reverts commit f3248b2237f999e0b6e6cc98d3d431abc64f566a.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5553,20 +5553,6 @@ public final class Settings {
         public static final String QS_TILE_STYLE = "qs_tile_style";
 
         /**
-         * Hold current font name and name of the originating package Uses "|" as a delimiter
-         *
-         * @hide
-         */
-        public static final String FONT_INFO = "font_info";
-
-        /**
-         * Hold list of installed font provider packages. Uses "|" as a delimiter
-         *
-         * @hide
-         */
-        public static final String FONT_PACKAGES = "font_packages";
-
-        /**
          * Change fonts for the system lockscreen date
          *
          * @hide
