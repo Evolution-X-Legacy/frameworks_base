65912d7c5ca5d23db6ed8b2ee1366d5375a7121f
Stallix <thestallix@gmail.com>
2019-08-11 18:54:36 +0530

    Revert "OMS: handle target or overlay package disabled"
    This reverts commit 8e05168045e4a89b4f21a941c31778460626243a.

--- services/core/java/com/android/server/om/OverlayManagerServiceImpl.java
+++ services/core/java/com/android/server/om/OverlayManagerServiceImpl.java
@@ -21,7 +21,6 @@ import static android.content.om.OverlayInfo.STATE_ENABLED;
 import static android.content.om.OverlayInfo.STATE_ENABLED_STATIC;
 import static android.content.om.OverlayInfo.STATE_MISSING_TARGET;
 import static android.content.om.OverlayInfo.STATE_NO_IDMAP;
-import static android.content.om.OverlayInfo.STATE_OVERLAY_NOT_AVAILABLE;
 import static android.content.om.OverlayInfo.STATE_OVERLAY_UPGRADING;
 import static android.content.om.OverlayInfo.STATE_TARGET_UPGRADING;
 
@@ -683,14 +682,6 @@ final class OverlayManagerServiceImpl {
 
         if (targetPackage == null) {
             return STATE_MISSING_TARGET;
-        }
-
-        if (!targetPackage.applicationInfo.enabled) {
-            return STATE_MISSING_TARGET;
-        }
-
-        if (!overlayPackage.applicationInfo.enabled) {
-            return STATE_OVERLAY_NOT_AVAILABLE;
         }
 
         if (!mIdmapManager.idmapExists(overlayPackage, userId)) {
